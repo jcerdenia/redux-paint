2160b9a63e05b9f8c6172d1c79bae6eaa21a4779
Joshua Cerdenia <joshcerdenia@gmail.com>
2022-01-02 16:54:14 +0800

    Add redux-logger middleware and test action

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,16 @@
 import { rootReducer } from "./rootReducer";
-import { devToolsEnhancer } from "redux-devtools-extension";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { logger } from "redux-logger";
 
 // Create and export a new store instance.
 // devToolsEnhancer is Redux DevTools middleware (gets triggered on each action dispatch).
 // It has access to the current action and the store, and can dispatch new actions.
 
-export const store = createStore(rootReducer, devToolsEnhancer({}));
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
+
+// Temporary dispatch for testing:
+store.dispatch({ type: "TEST_ACTION" });
